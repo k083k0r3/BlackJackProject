7f0e0f6d9a5537e5e5b57d07d3564a3c4152f3f7
k083k0r3 <kobekore@gmail.com>
2023-04-04 10:03:51 +0000

    New Dealer class to shuffle and dishOut cards to players

---
+++ src/main/java/org/example/Dealer.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Dealer {
+
+    public Dealer(){
+
+    }
+
+    public void dishOut(Deck cards, ArrayList<Player> players, int howManyCardsPerPlayer){
+        for (Player player : players) {
+            for (int numOfCards = 0; numOfCards < howManyCardsPerPlayer; numOfCards++) {
+                player.setHand(cards.getDeckPack().remove(0));
+            }
+
+        }
+    }
+
+    public void shuffleDeck(Deck cards){
+        Collections.shuffle(cards.getDeckPack());
+    }
+}
